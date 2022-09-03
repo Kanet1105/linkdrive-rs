fe5a68560ff27dd81cc3dd2dba9bf89a7f87992a
Allen Clarence <zombiedelah@gmail.com>
2022-09-04 00:42:48 +0900

    Remove the test of an obsolete function.

--- src/lib.rs
+++ src/lib.rs
@@ -16,22 +16,3 @@ pub fn run_app() -> Result<(), Exception> {
 
     Ok(())
 }
-
-#[test]
-pub fn test() -> Result<(), Exception> {
-    use std::time::Duration;
-    use headless_chrome::{Browser, LaunchOptionsBuilder};
-
-    let options = LaunchOptionsBuilder::default()
-        .headless(false)
-        .build()?;
-    let browser = Browser::new(options)?;
-    let tab = browser.wait_for_initial_tab()?;
-    tab.navigate_to("https://www.sciencedirect.com/search?qs=ai&show=100")?;
-    tab.wait_until_navigated()?;
-    
-    let outer_selector = "#main_content > div.SearchBody.row.transparent > div.transparent.results-container.col-xs-24.col-sm-16.col-lg-18.hidden-checkboxes.visible";
-    let result_list = tab.wait_for_element_with_custom_timeout(&outer_selector, Duration::from_millis(5000))?;
-    let a_list = result_list.wait_for_elements("a")?;
-    Ok(())
-}
