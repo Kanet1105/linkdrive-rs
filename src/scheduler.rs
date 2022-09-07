01a8d9e61e68ea444ad6e70b27ac0b543cd2257d
Allen Clarence <zombiedelah@gmail.com>
2022-09-07 20:22:26 +0900

    Move settings update that could happen on runtime inside 'Scheduler' struct.

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -1,11 +1,12 @@
+use std::collections::HashSet;
 use std::error::Error;
 use std::fmt::{Debug, Display};
 
 use chrono::prelude::*;
 use config::{self, Config};
 
-use super::load_config;
-use super::Exception;
+use crate::load_config;
+use crate::Exception;
 
 pub struct TimeFormatException((String, String));
 
@@ -78,12 +79,15 @@ impl Error for WeekdayException {}
 
 /// Scheduler struct.
 /// 
-/// "set_scheduler()", "default()" and "is_now()" are the
-/// only public functions that interfaces with a user.
+/// "update_scheduler()", "default()" and "is_now()" are 
+/// the only public interfaces in the struct.
 pub struct Scheduler {
     hour: u32,
     minute: u32,
     weekday: Weekday,
+    keyword: HashSet<String>,
+    id: Option<String>,
+    password: Option<String>,
 }
 
 impl Default for Scheduler {
@@ -92,12 +96,15 @@ impl Default for Scheduler {
             hour: 8,
             minute: 0,
             weekday: Weekday::Wed,
+            keyword: HashSet::<String>::new(),
+            id: None,
+            password: None,
         }
     }
 }
 
 impl Scheduler {
-    fn set_time(&mut self, config: &Config) -> Result<(), Exception> {
+    fn update_time(&mut self, config: &Config) -> Result<(), Exception> {
         let table = config.get_table("default")?;
         let alarm_time = table
             .get("time").unwrap()
@@ -125,7 +132,6 @@ impl Scheduler {
                     let message = "Set hour between 0 <= 'HH' < 24".to_string();
                     return Err(Box::new(TimeFormatException((message, hour.to_string()))))
                 }
-
                 self.hour = hour;
             },
             UnitTime::Minute => {
@@ -134,7 +140,6 @@ impl Scheduler {
                     let message = "Set minute between 0 <= 'MM' < 60".to_string();
                     return Err(Box::new(TimeFormatException((message, minute.to_string()))))
                 }
-
                 self.minute = minute;
             },
         }
@@ -142,7 +147,7 @@ impl Scheduler {
         Ok(())
     }
 
-    fn set_weekday(&mut self, config: &Config) -> Result<(), Exception> {
+    fn update_weekday(&mut self, config: &Config) -> Result<(), Exception> {
         let table = config.get_table("default")?;
         let weekday_value = table
             .get("weekday").unwrap()
@@ -163,10 +168,44 @@ impl Scheduler {
         Ok(())
     }
 
-    pub fn set_scheduler(&mut self) -> Result<(), Exception> {
+    fn update_keyword_and_email(&mut self, config: &Config) -> Result<(), Exception> {
+        let table = config.get_table("default")?;
+        let keyword: HashSet<String> = table
+            .get("keyword").unwrap()
+            .clone()
+            .into_array()?
+            .iter()
+            .map(|x| { x.to_string() })
+            .collect();
+        self.keyword = keyword;
+
+        Ok(())
+    }
+
+    /// This is where you can fail to send an email if you do not 
+    /// set your SMTP settings properly.
+    fn update_profile(&mut self, config: &Config) -> Result<(), Exception> {
+        let table = config.get_table("profile")?;
+        let (id, password): (String, String) = {
+            let id: String = table
+                .get("id").unwrap()
+                .to_string();
+            let password: String = table
+                .get("password").unwrap()
+                .to_string();
+            
+            (id, password)
+        };
+
+        Ok(())
+    }
+
+    pub fn update_scheduler(&mut self) -> Result<(), Exception> {
         let config = load_config()?;
-        self.set_time(&config)?;
-        self.set_weekday(&config)?;
+        self.update_time(&config)?;
+        self.update_weekday(&config)?;
+        self.update_keyword_and_email(&config)?;
+        self.update_profile(&config)?;
 
         Ok(())
     }
