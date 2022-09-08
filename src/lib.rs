9fb2c7639ef094c48449ee1b59723d8a9deb658b
Allen Clarence <zombiedelah@gmail.com>
2022-09-09 06:02:32 +0900

    A rough draft of the main loop consisting of nested conditional blocks.

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,12 @@
 mod crawler;
-mod scheduler;
 mod storage;
 
+use std::cell::RefCell;
 use std::env::current_dir;
 use std::path::PathBuf;
+use std::rc::Rc;
 use std::thread::sleep;
 use std::time::Duration;
-
-use chrono::prelude::*;
-use config::{self, Config};
 
 use crawler::ChromeDriver;
 
@@ -17,46 +15,60 @@ pub type Exception = Box<dyn std::error::Error>;
 
 /// The entry point of the app.
 pub fn run_app() -> Result<(), Exception> {
-    let mut flag = true;
-    // Initialize the crawler as a mutable reference.
-    let mut crawler = ChromeDriver::new()?;
+    // Initialize the crawler and the flag as a mutable reference.
+    let web_driver = ChromeDriver::new()?;
+    let crawler = Rc::new(RefCell::new(web_driver));
+    let flag = Rc::new(RefCell::new(false));
 
     loop {
-        crawler.search()?;
-        sleep(Duration::from_secs(2));
+        let mut crawler_mut = crawler.borrow_mut();
+        match crawler_mut.is_now() {
+            Ok(bool_value) => { if bool_value == true {
+                // Set the event off only when
+                // "bool_value" == true && "flag" == false.
+                let mut flag_mut = flag.borrow_mut();
+                if *flag_mut == false {
+                    match crawler_mut.search() {
+                        Ok(()) => {},
+                        Err(e) => { dbg!(e); }
+                    }
+                    *flag_mut = true;
+                    continue
+                } else {
+                    sleep(Duration::from_secs(1));
+                    continue
+                }
+            } else {
+                // Otherwise, set the flag back to false.
+                let mut flag_mut = flag.borrow_mut();
+                *flag_mut = false;
+            }},
+            Err(e) => { 
+                dbg!(e);
+                sleep(Duration::from_secs(1)); 
+            },
+        }
     }
 }
 
-/// Load configurations from the Settings.toml file located at
-/// the program root directory.
-fn load_config() -> Result<Config, Exception> {
-    // The base path for configs ("./Settings.toml").
-    let mut settings_path = current_dir()?;
-    settings_path.push("Settings.toml");
-    let settings_path_str = settings_path.to_str().unwrap();
+// /// Load configurations from the Settings.toml file located at
+// /// the program root directory.
+// fn load_config() -> Result<Config, Exception> {
+//     // The base path for configs ("./Settings.toml").
+//     let mut settings_path = current_dir()?;
+//     settings_path.push("Settings.toml");
+//     let settings_path_str = settings_path.to_str().unwrap();
 
-    // Build the config file.
-    let config = Config::builder()
-        .add_source(config::File::with_name(settings_path_str))
-        .build()?;
-
-    Ok(config)
-}
+//     // Build the config file.
+//     let config = Config::builder()
+//         .add_source(config::File::with_name(settings_path_str))
+//         .add_source(config::Environment::with_prefix("APP"))
+//         .build()?;
+//     Ok(config)
+// }
 
 fn load_csv_path() -> Result<PathBuf, Exception> {
     let mut csv_path = current_dir()?;
     csv_path.push("Papers.csv");
-
     Ok(csv_path)
-}
-
-/// Set the alarm off.
-fn is_now(alarm_time: (u32, u32, Weekday)) -> bool {
-    let local = Local::now();
-    let (h, m, w) = alarm_time;
-    if local.weekday() == w && local.hour() == h && local.minute() == m {
-        true
-    } else {
-        false
-    }
 }
